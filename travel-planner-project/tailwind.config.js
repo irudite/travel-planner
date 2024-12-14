9659268caac283b6871c39997e3ccbf69e4b2e67
irudite <KerrickTruong@gmail.com>
2024-12-14 13:08:28 -0500

    Fixed tailwind @apply error

--- travel-planner-project/tailwind.config.js
+++ travel-planner-project/tailwind.config.js
@@ -1,7 +1,9 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
     darkMode: ["class"],
-    content: ["./index.html", "./src/**/*.{ts,tsx,js,jsx}"],
+    content: ["./index.html", "./src/**/*.{js,jsx,html}", "./components/**/*.{js,jsx}",
+              "./app/**/*.{js,jsx}", "./pages/**/*.{js,jsx}",
+    ],
   theme: {
   	extend: {
   		borderRadius: {
