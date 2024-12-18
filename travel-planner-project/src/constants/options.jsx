353dd850eb274678c1c38cf34474d38acf74604e
irudite <KerrickTruong@gmail.com>
2024-12-18 03:14:35 -0500

    Finished design of creating trip page

---
+++ travel-planner-project/src/constants/options.jsx
@@ -0,0 +1,44 @@
+export const selectTravelSize=[
+    {
+        id: 1,
+        title: 'Me, Myself, and I',
+        desc: 'Some alone time exploration',
+        icon: '✈️',
+        people: '1'
+    },
+    {
+        id: 2,
+        title: 'Couple',
+        desc: 'Spend time with your significant other',
+        icon: '💑',
+        people: '2'
+    },
+    {
+        id: 3,
+        title: 'Family',
+        desc: 'Vacation with your family',
+        icon: '👪',
+        people: '3+'
+    }
+]
+
+export const selectTravelCost=[
+    {
+        id: 1,
+        title: 'Cheap',
+        desc: 'Try to keep prices low',
+        icon: '💵',
+    },
+    {
+        id: 2,
+        title: 'Rich',
+        desc: 'Can go above the assigned budget',
+        icon: '💰',
+    },
+    {
+        id: 3,
+        title: 'Luxury',
+        desc: 'No limits to budget',
+        icon: '🏦',
+    }
+]
--- travel-planner-project/src/create-itenary/index.jsx
+++ travel-planner-project/src/create-itenary/index.jsx
@@ -1,12 +1,27 @@
-import React from 'react'
+import React, { useEffect } from 'react'
 import GooglePlacesAutocomplete from 'react-google-places-autocomplete'
 import { useState } from 'react';
 import { Input } from '@/components/ui/input';
 import { Link } from 'react-router-dom';
 import { Button } from '@/components/ui/button';
+import { selectTravelCost, selectTravelSize } from '@/constants/options';
+
 
 function CreateItenary() {
   const [destination, setDestination] = useState();
+  const [tripData, setTripData] = useState([]);
+
+  const handleInputChange = (name, value)=> {
+    setTripData({
+      ...tripData,
+      [name]: value,
+    })
+  }
+
+  useEffect(() => {
+    console.log(tripData)
+  }, [tripData])
+
   return (
     <>
       <div className="sm:px-10 md:px-32 lg:px-56 xl:px-72 px-5 mt-10">
@@ -16,23 +31,62 @@ function CreateItenary() {
 
         <div className='mt-20'>
           <div>
-            <h2 className="text-[20px]"> Where would you like to go? </h2>
+            <h2 className="text-xl font-medium"> Where would you like to go? </h2>
             <GooglePlacesAutocomplete apiKey={import.meta.env.VITE_GOOGLE_PLACE_API_KEY}
               selectProps={{
                 destination, onChange: (d) => {
-                  setDestination(d)
+                  setDestination(d);
+                  handleInputChange('location', d);
                 }
               }} />
           </div>
         </div>
 
         <div classname='mt-20'>
-          <h2 className="text-xl"> How many days are going for? </h2>
-          <Input placeholder={"e.g. 1 day, 2 days"} type="number" />
+          <h2 className="text-xl my-3 font-medium"> How many days are going for? </h2>
+          <Input placeholder={"e.g. 1 day, 2 days"} type="number" 
+            onChange={(e)=> handleInputChange('noOfDays', e.target.value)}/>
         </div>
-        <Link to={'/'}>
-          <Button> Go Back </Button>
-        </Link>
+
+        <div>
+          <h2 className ="text-xl my-3 font-medium"> What is your budget for your trip? </h2>
+          <div className="grid grid-cols-3 gap-5 mt-5">
+              {selectTravelCost.map((item, index)=> (
+                <div key={index} className={`p-4 border rounded-lg hover:shadow-lg
+                  ${tripData?.budget==item.title&&'shadow-lg border-black'}`}
+                  onClick={()=>handleInputChange('budget', item.title)}>
+                  <h2 className="text-3xl">{item.icon}</h2>
+                  <h2 className="font-bold">{item.title}</h2>
+                  <h2 className="text-sm">{item.desc}</h2>
+                </div>
+              ))}
+          </div>
+        </div>
+
+        <div>
+          <h2 className ="text-xl my-3 font-medium"> How many people will be attending? </h2>
+          <div className="grid grid-cols-3 gap-5 mt-5">
+              {selectTravelSize.map((item, index)=> (
+                <div key={index} className={`p-4 border rounded-lg hover:shadow-lg
+                  ${tripData?.size==item.people&&'shadow-lg border-black'}`}
+                  onClick={()=>handleInputChange('size', item.people)}>
+                  <h2 className="text-3xl">{item.icon}</h2>
+                  <h2 className="font-bold">{item.title} | ({item.people})</h2>
+                  <h2 className="text-sm">{item.desc}</h2>
+                </div>
+              ))} 
+          </div>
+        </div>
+        
+        <div className="flex justify-end mt-5">
+          <Button> Generate Trip </Button>
+        </div>
+
+        <div className="flex ">
+          <Link to={'/'}>
+            <Button> Go Back </Button>
+          </Link>
+        </div>
 
       </div>
     </>
